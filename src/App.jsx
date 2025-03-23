7e9ec621f363a33884fd5c11938899c626c75806
Rahul Chauhan <rahulchauhan962103@gmail.com>
2025-03-23 17:29:23 +0530

    updating UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,36 @@ import './App.css'
 function App() {
 
   let [counter,setCounter]=useState(0);
+
+  //Function for Increament
   const increament=()=>{
-    setCounter(counter + 1);
+    counter=counter+1;
+    if(counter>20)
+    {
+      counter=20;
+    }
+    setCounter(counter);
   }
+
+  //Function for decreament
   const decreament=()=>{
-    setCounter(counter - 1);
+    counter=counter-1;
+    if(counter<0)
+    {
+      counter=0;
+    }
+    setCounter(counter);
   }
   return (
-    <>
-      <h1>Counter App</h1>
+    < >
       <div>
-        <button onClick={increament}>Increament</button>
-        <div>{counter}</div>
-        <button onClick={decreament}>Increament</button>
+        <h1>Counter App</h1>
+        <div className=''>
+          <button onClick={increament}>Increament</button>
+          <div>{counter}</div>
+          <button onClick={decreament}>Increament</button>
 
+        </div>
       </div>
     </>
   )
